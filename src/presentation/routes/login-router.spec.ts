696293cf355f5cda4a00b873b98b13d0675ba8a3
Hítalo Bruno <hitalobruno2011@gmail.com>
2023-05-31 13:53:24 -0300

    add jest watch script

--- src/presentation/routes/login-router.spec.ts
+++ src/presentation/routes/login-router.spec.ts
@@ -16,6 +16,11 @@ class LoginRouter {
         if (!httpRequest.body.email) {
             return { statusCode: 400 };
         }
+
+        if (!httpRequest.body.password) {
+            return { statusCode: 400 };
+        }
+
         return { statusCode: 200 };
     }
 }
@@ -27,6 +32,19 @@ describe('Login Router', () => {
             body: {
                 password: 'any_password',
                 email: '',
+            },
+        };
+
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
+
+    it('Should return 400 if no password is provided', async () => {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: {
+                password: '',
+                email: 'any_email@mail.com',
             },
         };
 

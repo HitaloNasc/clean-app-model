73c5d828c210afcbd41eea1c0654e9056b98b06c
Hítalo Bruno <hitalobruno2011@gmail.com>
2023-05-31 13:50:19 -0300

    lint login router

--- src/presentation/routes/login-router.spec.ts
+++ src/presentation/routes/login-router.spec.ts
@@ -1,37 +1,36 @@
 type LoginRequest = {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 };
 
 type HttpRequest = {
-  body: LoginRequest;
+    body: LoginRequest;
 };
 
 type HttpResponse = {
-  statusCode: number;
+    statusCode: number;
 };
 
 class LoginRouter {
-  public route(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.email) {
-      return { statusCode: 400 }
+    public route(httpRequest: HttpRequest): HttpResponse {
+        if (!httpRequest.body.email) {
+            return { statusCode: 400 };
+        }
+        return { statusCode: 200 };
     }
-
-    return { statusCode: 200 }
-  }
 }
 
-describe("Login Router", () => {
-  it("Should return 400 if no email is provided", async () => {
-    const sut = new LoginRouter();
-    const httpRequest = {
-      body: {
-        password: "any_password",
-        email: "",
-      },
-    };
+describe('Login Router', () => {
+    it('Should return 400 if no email is provided', async () => {
+        const sut = new LoginRouter();
+        const httpRequest = {
+            body: {
+                password: 'any_password',
+                email: '',
+            },
+        };
 
-    const httpResponse = sut.route(httpRequest);
-    expect(httpResponse.statusCode).toBe(400);
-  });
+        const httpResponse = sut.route(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+    });
 });

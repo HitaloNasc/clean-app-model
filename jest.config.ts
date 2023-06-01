1f61858c40ebc0ad66526af48ffbe01b383d371e
Hítalo Bruno <hitalobruno2011@gmail.com>
2023-06-01 15:22:07 -0300

    fix jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
 export default {
-  roots: ["<rootDir>/src"],
-  testEnvironment: "node",
-  transform: {
-    ".+\\.ts$": "ts-jest",
-  },
-  moduleNameMapper: {
-    "@/(.*)": "<rootDir>/src/$1",
-  },
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+    roots: ['<rootDir>/src'],
+    testEnvironment: 'node',
+    transform: {
+        '.+\\.ts$': 'ts-jest',
+    },
+    moduleNameMapper: {
+        '@/(.*)': '<rootDir>/src/$1',
+    },
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
 };
